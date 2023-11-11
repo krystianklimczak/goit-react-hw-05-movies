802fc249c5eb343d8e85ab6e743eb878d23ff67f
krystianklimczak <krystianklimczak1996r.com>
2023-11-11 20:56:20 +0100

    add all components to app with logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Home from 'pages/home/Home';
+import Movies from 'pages/movies/Movies';
+import MovieDetails from './movieDetails/MovieDetails';
+import Cast from './cast/Cast';
+import Reviews from './reviews/Reviews';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<Home />} />
+      </Routes>
     </div>
   );
 };
